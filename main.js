3e752b8b0198aaf6fc5a41f756763ae76071e0a9
root <root@LAPTOP-MBSPIFLM.localdomain>
2022-04-13 18:38:59 -0700

    loops done

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 //Initialize game board on page load
+initCatRow()
 initBoard()
+
+function initCatRow(){
+    let catRow = document.getElementById('category-row')
+
+    for(let i = 0; i < 6; i++){
+        let box = document.createElement('div')
+        box.className = 'clue-box category-box'
+        catRow.appendChild(box)
+    }
+}
 
 function initBoard(){
     let board = document.getElementById('clue-board')
 
     //generate 5 rows of clues, then place 6 boxes in each row
 
-    for(let i = 0; i < 5;i++){
+    for(let i = 0; i < 5; i++){
         let row = document.createElement('div')
         let boxValue = 200 * (i + 1)
         row.className = 'clue-row'
 
-        for(let j = 0; j < 6;j++){
+        for(let j = 0; j < 6; j++){
             let box = document.createElement('div')
             box.className = 'clue-box'
             box.textContent = '$' + boxValue
